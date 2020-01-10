e20ec8a35a27dfe40ee4bace9180c78cc503cb45
unknown <nelsondiasdemedeiros@gmail.com>
2020-01-10 21:50:55 -0200

    WIP: adicionando toast de aviso, quando não tiver endereço e creditcard cadastrado

--- bookstore/src/app/components/carts/cart/cart.component.ts
+++ bookstore/src/app/components/carts/cart/cart.component.ts
@@ -1,3 +1,5 @@
+import { CreditCardService } from './../../../core/services/credit-card.service';
+import { AddressService } from './../../../core/services/address.service';
 import { LoginComponent } from './../../login/login.component';
 import { MatDialog } from '@angular/material';
 import { AuthService } from './../../../core/services/auth.service';
@@ -5,6 +7,7 @@ import { CartService } from 'src/app/core/services/cart.service';
 import { CartBaseComponent } from './../cart-popup/cart-base.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cart',
@@ -14,8 +17,13 @@ import { Router } from '@angular/router';
 
 export class CartComponent extends CartBaseComponent  {
 
-  constructor(protected cartService: CartService,
+  private currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+  constructor(private creditCardService: CreditCardService,
+              private addressService: AddressService,
+              protected cartService: CartService,
               private authService: AuthService,
+              private toastr: ToastrService,
               public dialog: MatDialog,
               private router: Router) {
 
@@ -29,7 +37,24 @@ export class CartComponent extends CartBaseComponent  {
 
   checkout(): void {
     if (this.authService.loggedIn()) {
-      this.router.navigate(['carts/checkouts']);
+
+      if (this.currentUser.address &&
+        this.currentUser.credit_card) {
+        this.router.navigate(['carts/checkouts']);
+
+      } else {
+
+        this.router.navigate(['accounts']);
+
+        this.toastr.error('Register your address and credit card in your account.', null, {
+          progressAnimation: 'decreasing',
+          positionClass: 'toast-center-center',
+          progressBar: true,
+          closeButton: true,
+          timeOut: 15000,
+        });
+      }
+
     } else {
       this.openLoginDialog();
     }
